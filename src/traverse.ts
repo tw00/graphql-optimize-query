91936b24143300632c69a6d2f53e3cf87794cc2b
Thomas Weustenfeld <tw@econify.com>
2021-07-06 16:12:58 -0500

    :art: Implemented optimization for non-filter case

--- src/traverse.ts
+++ src/traverse.ts
@@ -7,35 +7,78 @@ import {
   VariableDefinitionNode,
 } from 'graphql';
 
+enum TreeOperation {
+  NOOP = 0,
+  REMOVE = 1,
+  KEEP = 2,
+}
+
 export default function evaluateIncludeStatements(
   query: ASTNode,
   variables: Record<string, boolean>,
 ): ASTNode {
-  function checkIncludeDirective(directive: DirectiveNode): boolean {
+  function checkDirective(directive: DirectiveNode): TreeOperation {
     const checkVar = (varName, value) => variables[varName] === value;
 
-    const checkInclude = (d) =>
+    const checkInclude = (d, v) =>
       d.kind === 'Directive' &&
       d.name.value === 'include' &&
-      checkVar(d.arguments[0].value.name.value, false);
+      checkVar(d.arguments[0].value.name.value, v);
 
-    const checkSkip = (d) =>
+    const checkSkip = (d, v) =>
       d.kind === 'Directive' &&
       d.name.value === 'skip' &&
-      checkVar(d.arguments[0].value.name.value, true);
+      checkVar(d.arguments[0].value.name.value, v);
 
-    return checkInclude(directive) || checkSkip(directive);
+    if (checkInclude(directive, false) || checkSkip(directive, true)) {
+      return TreeOperation.REMOVE;
+    }
+
+    if (checkInclude(directive, true) || checkSkip(directive, false)) {
+      return TreeOperation.KEEP;
+    }
+
+    return TreeOperation.NOOP;
   }
 
-  function CleanSubTree(
-    node: InlineFragmentNode | FieldNode,
-  ): null | undefined {
-    if (node.directives?.some(checkIncludeDirective)) {
+  function CleanSubTree<T extends InlineFragmentNode | FieldNode>(
+    node: T,
+  ): T | null | undefined {
+    const operations = node.directives?.map(checkDirective) ?? [];
+    const operationsWithoutNoop = operations.filter(
+      (op) => op !== TreeOperation.NOOP,
+    );
+
+    if (
+      operationsWithoutNoop.includes(TreeOperation.REMOVE) &&
+      operationsWithoutNoop.includes(TreeOperation.KEEP)
+    ) {
+      throw new Error(
+        'Operation can not have contradicting @include and @skip directives.',
+      );
+    }
+
+    if (operationsWithoutNoop[0] === TreeOperation.REMOVE) {
       return null;
+    }
+
+    if (operationsWithoutNoop[0] === TreeOperation.KEEP) {
+      return {
+        ...node,
+        directives: node.directives.filter(
+          (_d, i) =>
+            !(
+              operations[i] === TreeOperation.REMOVE ||
+              operations[i] === TreeOperation.KEEP
+            ),
+        ),
+      };
     }
   }
 
-  function CleanVarDefinition(node: VariableDefinitionNode): null | undefined {
+  function CleanVarDefinition(
+    node: VariableDefinitionNode,
+  ): null | InlineFragmentNode | FieldNode | undefined {
     if (Object.keys(variables).includes(node.variable.name.value)) {
       return null;
     }
--- test/traverse.test.ts
+++ test/traverse.test.ts
@@ -2,23 +2,68 @@ import { print } from 'graphql';
 import gql from 'graphql-tag';
 import evaluateIncludeStatements from '../src/traverse';
 
+const testQueryBasic = gql`
+  query Test($enable: Boolean!) {
+    test {
+      field @include(if: $enable)
+      nested @include(if: $enable) {
+        field
+      }
+      ... on Type @include(if: $enable) {
+        typeField
+      }
+      unrelated1 @unrelated
+      unrelated2 @unrelated @skip(if: $enable)
+    }
+  }
+`;
+
+const testQueryInclude = gql`
+  query GetUser($userID: ID!, $auth: Boolean!) {
+    user(id: $userID) {
+      name @include(if: $auth) {
+        firstName
+        lastName
+      }
+      ... on Asset @include(if: $auth) {
+        data
+      }
+      other
+    }
+  }
+`;
+
+const testQuerySkip = gql`
+  query GetUser($userID: ID!, $noAuth: Boolean!) {
+    user(id: $userID) {
+      name @skip(if: $noAuth) {
+        firstName
+        lastName
+      }
+      ... on Asset @skip(if: $noAuth) {
+        data
+      }
+      other
+    }
+  }
+`;
+
 describe('test evaluateIncludeStatements', (): void => {
-  test('include', (): void => {
-    const testQuery = gql`
-      query GetUser($userID: ID!, $auth: Boolean!) {
-        user(id: $userID) {
-          name @include(if: $auth) {
-            firstName
-            lastName
-          }
-          ... on Asset @include(if: $auth) {
-            data
-          }
-          other
+  test('basic test', (): void => {
+    const ref = gql`
+      query Test {
+        test {
+          unrelated1 @unrelated
+          unrelated2 @unrelated
         }
       }
     `;
 
+    const result = evaluateIncludeStatements(testQueryBasic, { enable: false });
+    expect(print(result)).toBe(print(ref));
+  });
+
+  test('include (removing)', (): void => {
     const ref = gql`
       query GetUser($userID: ID!) {
         user(id: $userID) {
@@ -27,26 +72,31 @@ describe('test evaluateIncludeStatements', (): void => {
       }
     `;
 
-    const result = evaluateIncludeStatements(testQuery, { auth: false });
+    const result = evaluateIncludeStatements(testQueryInclude, { auth: false });
+    expect(print(result)).toBe(print(ref));
+  });
+
+  test('include (keeping)', (): void => {
+    const ref = gql`
+      query GetUser($userID: ID!) {
+        user(id: $userID) {
+          name {
+            firstName
+            lastName
+          }
+          ... on Asset {
+            data
+          }
+          other
+        }
+      }
+    `;
+
+    const result = evaluateIncludeStatements(testQueryInclude, { auth: true });
     expect(print(result)).toBe(print(ref));
   });
 
   test('skip', (): void => {
-    const testQuery = gql`
-      query GetUser($userID: ID!, $noAuth: Boolean!) {
-        user(id: $userID) {
-          name @skip(if: $noAuth) {
-            firstName
-            lastName
-          }
-          ... on Asset @skip(if: $noAuth) {
-            data
-          }
-          other
-        }
-      }
-    `;
-
     const ref = gql`
       query GetUser($userID: ID!) {
         user(id: $userID) {
@@ -55,7 +105,44 @@ describe('test evaluateIncludeStatements', (): void => {
       }
     `;
 
-    const result = evaluateIncludeStatements(testQuery, { noAuth: true });
+    const result = evaluateIncludeStatements(testQuerySkip, { noAuth: true });
     expect(print(result)).toBe(print(ref));
+  });
+
+  test('contradiction 1', () => {
+    const testQueryInvalid = gql`
+      query GetUser($userID: ID!, $auth: Boolean!, $noAuth: Boolean!) {
+        user(id: $userID) {
+          name @include(if: $auth) @include(if: $noAuth) {
+            foobar
+          }
+        }
+      }
+    `;
+
+    expect(() => {
+      evaluateIncludeStatements(testQueryInvalid, {
+        auth: true,
+        noAuth: false,
+      });
+    }).toThrowError();
+  });
+
+  test('contradiction 2', () => {
+    const testQueryInvalid = gql`
+      query GetUser($userID: ID!, $auth: Boolean!) {
+        user(id: $userID) {
+          name @include(if: $auth) @skip(if: $auth) {
+            foobar
+          }
+        }
+      }
+    `;
+
+    expect(() => {
+      evaluateIncludeStatements(testQueryInvalid, {
+        auth: true,
+      });
+    }).toThrowError();
   });
 });
